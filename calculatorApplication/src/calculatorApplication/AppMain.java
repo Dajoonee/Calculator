a547b996e147faa3be28843d75469df038e1c9b0
shyani20 <shyani20@gmail.com>
2020-03-21 18:22:09 +0900

    Add Main Class [AppMain.java]

---
+++ calculatorApplication/src/calculatorApplication/AppMain.java
@@ -0,0 +1,22 @@
+package calculatorApplication;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class AppMain extends Application {
+	
+	@Override
+	public void start(Stage ps) throws Exception {
+		
+		
+	}
+
+	public static void main(String[] args) {
+		
+		launch(args);
+		
+	}
+
+	
+
+}
