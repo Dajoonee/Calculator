0df21b35ea58ace4e5863cde9da9bc2cd84dd9db
shyani20 <shyani20@gmail.com>
2020-03-21 20:01:35 +0900

    Update code [AppMain.java]

--- calculatorApplication/src/calculatorApplication/AppMain.java
+++ calculatorApplication/src/calculatorApplication/AppMain.java
@@ -1,13 +1,21 @@
 package calculatorApplication;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class AppMain extends Application {
 	
 	@Override
 	public void start(Stage ps) throws Exception {
+		Parent root = FXMLLoader.load(getClass().getResource("MainUI.fxml"));
+		Scene sc = new Scene(root);
 		
+		ps.setTitle("Calculator");
+		ps.setScene(sc);
+		ps.show();
 		
 	}
 
