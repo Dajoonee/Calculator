c0dbc11fb83c3e82be4f8b0c3338391efab36a76
Dajoonee <shyani20@gmail.com>
2020-03-23 01:15:15 +0900

    Update code in [MainUI.fxml & CalController.java] Addition of two numbers[1,2]

--- calculatorApplication/src/calculatorApplication/CalController.java
+++ calculatorApplication/src/calculatorApplication/CalController.java
@@ -1,6 +1,7 @@
 package calculatorApplication;
 
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 
 import javafx.event.ActionEvent;
@@ -8,39 +9,89 @@ import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 
 public class CalController implements Initializable {
 	
-	@FXML Button btn0,btn2,btn3,btn4,btn5,btn6,btn7,btn8,btn9;
-	@FXML Button btn;
+	@FXML Button btn0,btn1,btn2,btn3,btn4,btn5,btn6,btn7,btn8,btn9;
+
 	@FXML Button btnPoint;
 	@FXML Button btnCencle;
 	@FXML Button btnDelete;
 	@FXML Button btnResult;
 	@FXML Button btnPlus, btnMinus, btnx, btnDivide;
+	@FXML Label result;
+	String showNumber="";
+	String theNum ="";
+	int number = 0;
+	ArrayList<String> num = new ArrayList<String>();
+	ArrayList <String> operator = new ArrayList<String>();
+	int i = 0;
 	
 	@Override
 	public void initialize(URL location, ResourceBundle resource) {
-		btn.setOnAction(new EventHandler<ActionEvent>() {
+		btn1.setOnAction(new EventHandler<ActionEvent>() {
+			
 			@Override
 			public void handle(ActionEvent event) {
-				inputNum(event);
+				getNum1(event);
 			}
+			
 		});
-		
+		btn2.setOnAction(event->getNum2(event));
+		btnPlus.setOnAction(event->plus(event));
+		btnResult.setOnAction(event->result(event));
 		
 	}
 	
-	public void inputNum(ActionEvent e) {
-		String btnNumber = btn.getText();
-		printString(btnNumber);
+	public void getNum1(ActionEvent e) {
+		String btnNumber = btn1.getText();
+		printString(showNumber);
+		showNumber += btnNumber;
+		theNum += btnNumber;
+		result.setText(showNumber);
+
 		
+	}
+	public void getNum2(ActionEvent e) {
+		String btnNumber = btn2.getText();
+		printString(showNumber);
+		showNumber += btnNumber;
+		theNum += btnNumber;
+		result.setText(showNumber);
+		
+	}
+	
+	public void plus(ActionEvent e) {
+		showNumber += btnPlus.getText();
+		result.setText(showNumber);
+		printString(showNumber);
+		number += (Integer.parseInt(theNum));
+		operator.add("+");
+		theNum ="";
+	}
+	
+	public void result(ActionEvent e) {
+		if(operator.get(operator.size()-1)=="+"){
+			
+			number += Integer.parseInt(theNum);
+			result.setText(Integer.toString(number));
+			showNumber="";
+			number = 0;
+			for(int i=0; i<operator.size(); i++) {
+				operator.remove(i);
+			}
+		}
 	}
 	
 	public void printString(String num) {
 		if(num!=null) {
-			System.out.println(num);
+			System.out.print(num);
 		}
+	}
+	
+	public void calculation() {
+		
 	}
 
 }
