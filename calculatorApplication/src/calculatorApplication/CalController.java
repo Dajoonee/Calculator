ceb71278c77f8c606cd5ebb3c8e5647c8d60346b
Dajoonee <shyani20@gmail.com>
2020-03-23 02:29:28 +0900

    New Logic [Calcontroller.java]

--- calculatorApplication/src/calculatorApplication/CalController.java
+++ calculatorApplication/src/calculatorApplication/CalController.java
@@ -21,77 +21,53 @@ public class CalController implements Initializable {
 	@FXML Button btnResult;
 	@FXML Button btnPlus, btnMinus, btnx, btnDivide;
 	@FXML Label result;
-	String showNumber="";
-	String theNum ="";
-	int number = 0;
-	ArrayList<String> num = new ArrayList<String>();
-	ArrayList <String> operator = new ArrayList<String>();
-	int i = 0;
+	String outputVar = "";
+	
+	ArrayList<String> getOper = new ArrayList<String>();
+	ArrayList<String> getNumber = new ArrayList<String>();
+	
+	int finalResult = 0;
+	
 	
 	@Override
 	public void initialize(URL location, ResourceBundle resource) {
-		btn1.setOnAction(new EventHandler<ActionEvent>() {
-			
-			@Override
-			public void handle(ActionEvent event) {
-				getNum1(event);
-			}
-			
-		});
-		btn2.setOnAction(event->getNum2(event));
-		btnPlus.setOnAction(event->plus(event));
-		btnResult.setOnAction(event->result(event));
 		
+		btn0.setOnAction(event->clickBtn0(event));
+		btn1.setOnAction(event->clickBtn1(event));
+		btn2.setOnAction(event->clickBtn2(event));
+		btn3.setOnAction(event->clickBtn3(event));
+		btn4.setOnAction(event->clickBtn4(event));
+		btn5.setOnAction(event->clickBtn5(event));
+		btn6.setOnAction(event->clickBtn6(event));
+		btn7.setOnAction(event->clickBtn7(event));
+		btn8.setOnAction(event->clickBtn8(event));
+		btn9.setOnAction(event->clickBtn9(event));
+	
+	
 	}
 	
-	public void getNum1(ActionEvent e) {
-		String btnNumber = btn1.getText();
-		printString(showNumber);
-		showNumber += btnNumber;
-		theNum += btnNumber;
-		result.setText(showNumber);
-
-		
-	}
-	public void getNum2(ActionEvent e) {
-		String btnNumber = btn2.getText();
-		printString(showNumber);
-		showNumber += btnNumber;
-		theNum += btnNumber;
-		result.setText(showNumber);
-		
+	// 0~9 버튼 클릭 이벤트 핸들러
+	void clickBtn0(ActionEvent e) { String inputVar = btn0.getText(); saveInputVar(inputVar);}
+	void clickBtn1(ActionEvent e) { String inputVar = btn1.getText(); saveInputVar(inputVar);}
+	void clickBtn2(ActionEvent e) { String inputVar = btn2.getText(); saveInputVar(inputVar);}
+	void clickBtn3(ActionEvent e) { String inputVar = btn3.getText(); saveInputVar(inputVar);}
+	void clickBtn4(ActionEvent e) { String inputVar = btn4.getText(); saveInputVar(inputVar);}
+	void clickBtn5(ActionEvent e) { String inputVar = btn5.getText(); saveInputVar(inputVar);}
+	void clickBtn6(ActionEvent e) { String inputVar = btn6.getText(); saveInputVar(inputVar);}
+	void clickBtn7(ActionEvent e) { String inputVar = btn7.getText(); saveInputVar(inputVar);}
+	void clickBtn8(ActionEvent e) { String inputVar = btn8.getText(); saveInputVar(inputVar);}
+	void clickBtn9(ActionEvent e) { String inputVar = btn9.getText(); saveInputVar(inputVar);}
+	
+	// 연산 버튼 클릭 이벤트 처리	
+	void clickBtnPlus(ActionEvent e) { String inputVar = btnPlus.getText(); saveInputVar(inputVar);}
+	void clickBtnMinus(ActionEvent e) { String inputVar = btnMinus.getText(); saveInputVar(inputVar);}
+	void clickBtnx(ActionEvent e) { String inputVar = btnx.getText(); saveInputVar(inputVar);}
+	void clickBtnDivide(ActionEvent e) { String inputVar = btnDivide.getText(); saveInputVar(inputVar);}
+	
+	void saveInputVar(String var) {
+		outputVar += var;
+		result.setText(outputVar);
 	}
 	
-	public void plus(ActionEvent e) {
-		showNumber += btnPlus.getText();
-		result.setText(showNumber);
-		printString(showNumber);
-		number += (Integer.parseInt(theNum));
-		operator.add("+");
-		theNum ="";
-	}
-	
-	public void result(ActionEvent e) {
-		if(operator.get(operator.size()-1)=="+"){
-			
-			number += Integer.parseInt(theNum);
-			result.setText(Integer.toString(number));
-			showNumber="";
-			number = 0;
-			for(int i=0; i<operator.size(); i++) {
-				operator.remove(i);
-			}
-		}
-	}
-	
-	public void printString(String num) {
-		if(num!=null) {
-			System.out.print(num);
-		}
-	}
-	
-	public void calculation() {
-		
-	}
 
 }
